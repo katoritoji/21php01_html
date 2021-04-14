ce17d55e934956869ab55cf2a6b7a83fdd05a53c
Anh Dũng <anhdung.mit2809@gmail.com>
2021-04-14 18:30:01 +0700

    ss11

--- Ss11/BTVN/form/js/form.js
+++ Ss11/BTVN/form/js/form.js
@@ -7,23 +7,31 @@ function validate() {
 		document.getElementById("error-name").innerText = "Vui lòng nhập tên của bạn";
 		document.getElementById("error-name").style.color = "red";
 		document.getElementById("error-name").style.fontStyle = "italic";
+	} else {
+		document.getElementById("error-name").innerText = "";
 	}
 	
 	if (email == ''){
 		document.getElementById("error-email").innerText = "Vui lòng nhập email";
 		document.getElementById("error-email").style.color = "red";
 		document.getElementById("error-email").style.fontStyle = "italic";
+	} else {
+		document.getElementById("error-email").innerText = "";
 	}
 	
 	if (title == ''){
 		document.getElementById("error-title").innerText = "Vui lòng nhập tiêu đề";
 		document.getElementById("error-title").style.color = "red";
 		document.getElementById("error-title").style.fontStyle = "italic";
+	} else {
+		document.getElementById("error-title").innerText = "";
 	}
 	
 	if (content == ''){
 		document.getElementById("error-content").innerText = "Vui lòng nhập nội dung";
 		document.getElementById("error-content").style.color = "red";
 		document.getElementById("error-content").style.fontStyle = "italic";
+	} else {
+		document.getElementById("error-content").innerText = "";
 	}
 }
